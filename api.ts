52c4b9a5b8db9079cc6116f1962304a7c3e0369d
Pierre Golfier <pgolfier.pro@gmail.com>
2021-08-22 22:14:57 +0200

    update

---
+++ api.ts
@@ -0,0 +1,79 @@
+export interface CoinApiOptions {
+  converts: string[];
+  symbols: string[];
+}
+
+export class CoinApi {
+  options: CoinApiOptions;
+  constructor(
+    options: CoinApiOptions = {
+      converts: ["EUR", "USD"],
+      symbols: ["BTC", "ETH", "ADA", "LINK"],
+    },
+  ) {
+    this.options = options;
+  }
+
+  async call() {
+    // deno-lint-ignore no-explicit-any
+    const results: any = {};
+    for await (const convert of this.options.converts) {
+      const url =
+        `https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol=${
+          this.options.symbols.join(",")
+        }&convert=${convert}`;
+      const req = await fetch(
+        url,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            "X-CMC_PRO_API_KEY": Deno.env.get("API_TOKEN")!,
+          },
+        },
+      );
+      const coins = (await req.json()).data;
+      const coinsKeys = Object.keys(coins);
+
+      coinsKeys.forEach((key) => {
+        const coin = coins[key];
+        if (results[key]) {
+          results[key].quote = { ...results[key].quote, ...coin.quote };
+        } else {
+          results[key] = {
+            name: coin.name,
+            symbol: coin.symbol,
+            quote: coin.quote,
+          };
+        }
+      });
+    }
+
+    const coinsKeys = Object.keys(results);
+
+    const text = coinsKeys.map((c) => {
+      const coin = results[c];
+      const eurQuote = coin.quote.EUR;
+      const usdQuote = coin.quote.USD;
+      return `**${results[c].name} (${c}) :**
+       *$${this.truncate(usdQuote.price)} / ${this.truncate(eurQuote.price)}€*
+       Jour: ${this.truncate(usdQuote.percent_change_24h)}% ${
+        this.growth(usdQuote.percent_change_24h)
+      }
+       Sem.: ${this.truncate(usdQuote.percent_change_7d)}% ${
+        this.growth(usdQuote.percent_change_7d)
+      }
+       `;
+    }).join("\n");
+
+    return text;
+  }
+
+  truncate(val: number) {
+    return Math.round(val * 100) / 100;
+  }
+
+  growth(val: number) {
+    if (val > 0) return ":greencircle:";
+    else return ":redcircle:";
+  }
+}
---
+++ deps.ts
@@ -0,0 +1,13 @@
+// Sift is a small routing library that abstracts the details like registering
+// a fetch event listener and provides a simple function (serve) that has an
+// API to invoke a function for a specific path.
+export {
+  json,
+  serve,
+  validateRequest,
+} from "https://deno.land/x/sift@0.1.7/mod.ts";
+// TweetNaCl is a cryptography library that we use to verify requests
+// from Discord.
+import nacl from "https://cdn.skypack.dev/tweetnacl@v1.0.3?dts";
+
+export { nacl }
