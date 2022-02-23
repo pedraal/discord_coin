8def8d9cac943730860771b645e4c89a44202d58
Pierre Golfier <pgolfier.pro@gmail.com>
2022-02-23 13:54:35 +0100

    try different static asset serving method for prod

--- mod.ts
+++ mod.ts
@@ -1,12 +1,32 @@
-import { serve, serveStatic } from "./deps.ts";
+import { serve } from "./deps.ts";
 import { discordCommandsHandler } from "./services/discord_commands.ts";
 import { reportHandler } from "./services/report.ts";
-
-console.log(import.meta.url);
-
+import { contentType } from "https://deno.land/x/media_types/mod.ts";
+import * as path from "https://deno.land/std/path/mod.ts";
 serve({
   "/discord": discordCommandsHandler,
-  "/": serveStatic("public/index.html", { baseUrl: import.meta.url }),
+  "/": handleStaticAsset,
   "/api": reportHandler,
-  "/:filename+": serveStatic("public", { baseUrl: import.meta.url }),
+  "/:filename+": handleStaticAsset,
 });
+
+async function handleStaticAsset(request: Request): Promise<Response> {
+  const { pathname } = new URL(request.url);
+
+  if (pathname === "/") {
+    const file = await Deno.readFile(path.join("public", "index.html"));
+    return new Response(file);
+  }
+
+  const type = contentType(path.extname(pathname));
+  const file = await Deno.readFile(path.join("public", pathname));
+
+  if (!type) {
+    throw new Error("invalid content type requested");
+  }
+  return new Response(file, {
+    headers: {
+      "content-type": type,
+    },
+  });
+}
