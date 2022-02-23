66c927c6796c410134b875ed9036f9de8b2ddc20
Pierre Golfier <pgolfier.pro@gmail.com>
2022-02-23 13:37:01 +0100

    add debug for staging

--- mod.ts
+++ mod.ts
@@ -2,9 +2,11 @@ import { serve, serveStatic } from "./deps.ts";
 import { discordCommandsHandler } from "./services/discord_commands.ts";
 import { reportHandler } from "./services/report.ts";
 
+console.log(import.meta.url);
+
 serve({
   "/discord": discordCommandsHandler,
-  "/report": serveStatic("public/index.html", { baseUrl: import.meta.url }),
+  "/": serveStatic("public/index.html", { baseUrl: import.meta.url }),
   "/api": reportHandler,
   "/:filename+": serveStatic("public", { baseUrl: import.meta.url }),
 });
